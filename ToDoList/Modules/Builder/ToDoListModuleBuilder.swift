defc7a323d125ed159a30722fb68fef0c2c73271
Alexander6Shevtsov <axrsvr@gmail.com>
2025-09-10 21:30:49 +0300

    fix Builder

--- ToDoList/Modules/Builder/ToDoListModuleBuilder.swift
+++ ToDoList/Modules/Builder/ToDoListModuleBuilder.swift
@@ -6,30 +6,27 @@
 //
 
 import UIKit
-import CoreData
 
 // MARK: - ToDoListModuleBuilder
 enum ToDoListModuleBuilder {
     
-    static func build() -> UIViewController {
-        let container = (UIApplication.shared.delegate as? AppDelegate)?.persistentContainer
-        let repository = ToDoRepository(persistentContainer: container)
-
-        let interactor = ToDoListInteractor(repository: repository)
-        let router = ToDoListRouter()
+    // MARK: - Public Methods
+    static func build() -> ToDoListViewController {
         let viewController = ToDoListViewController()
+        let interactor = ToDoListInteractor()
+        let router = ToDoListRouter()
+        
         let presenter = ToDoListPresenter(
             view: viewController,
             interactor: interactor,
             router: router
         )
-
-        interactor.output = presenter
+        
         viewController.output = presenter
-
-        viewController.title = "Задачи"
-        viewController.navigationItem.backButtonDisplayMode = .generic
-        viewController.navigationItem.backButtonTitle = "Назад"
+        interactor.output = presenter
+        
+        viewController.title = "ToDo List"
+        
         return viewController
     }
 }
