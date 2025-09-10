ac2d195a6f787e41e191bdc6700edce85c5cc2a4
Alexander6Shevtsov <axrsvr@gmail.com>
2025-09-10 10:27:43 +0300

    inject NSPersistentContainer into ToDoRepository

--- ToDoList/Modules/Builder/ToDoListModuleBuilder.swift
+++ ToDoList/Modules/Builder/ToDoListModuleBuilder.swift
@@ -6,19 +6,27 @@
 //
 
 import UIKit
+import CoreData
 
+// MARK: - ToDoListModuleBuilder
 enum ToDoListModuleBuilder {
+    
     static func build() -> UIViewController {
-        let viewController = ToDoListViewController()
-        let repository = ToDoRepository()
+        let container = (UIApplication.shared.delegate as? AppDelegate)?.persistentContainer
+        
+        let repository = ToDoRepository(persistentContainer: container)
+        
+        // Слои VIPER
         let interactor = ToDoListInteractor(repository: repository)
         let router = ToDoListRouter()
+        let viewController = ToDoListViewController()
         let presenter = ToDoListPresenter(
             view: viewController,
             interactor: interactor,
             router: router
         )
         
+        // Связи
         interactor.output = presenter
         viewController.output = presenter
         
